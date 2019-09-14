68de3dfdd6bacf097df29098f80c86cf45e71c51
Vlad Gheorghe <vlad.gheorghe@gmail.com>
2019-09-14 14:21:18 +0300

    sample for string to enum cast

---
+++ concordion-iss-cast-enum/src/test/java/org/example/CastEnumFixture.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+
+public class CastEnumFixture {
+	
+	
+	public void doStuff(Color c) {
+		// stuff here
+	}
+
+
+}
---
+++ concordion-iss-cast-enum/src/test/java/org/example/Color.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public enum Color {
+	RED,
+	GREEN,
+	BLUE
+}
