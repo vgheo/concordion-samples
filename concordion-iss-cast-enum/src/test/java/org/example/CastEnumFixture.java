7e9e6028b4db6aafd064d1a404bb126faea04407
Vlad Gheorghe <vlad.gheorghe@gmail.com>
2019-09-14 14:37:49 +0300

    set enum property

--- concordion-iss-cast-enum/src/test/java/org/example/CastEnumFixture.java
+++ concordion-iss-cast-enum/src/test/java/org/example/CastEnumFixture.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import org.concordion.api.FullOGNL;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(ConcordionRunner.class)
-
+@FullOGNL
 public class CastEnumFixture {
 	
+	private Color var1;
+	public Color var2;
+	
+	public void setVar1(Color c) {
+		var1=c;
+	}
 	
 	public void doStuff(Color c) {
 		// stuff here
